ad2ced97dd8f94e4e45e14de287a0ca8b885b393
dpchennev <122350071+dpchennev@users.noreply.github.com>
2023-09-01 08:45:16 -0700

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faMedium,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
+import { Box, HStack } from "@chakra-ui/react";
+
+const socials = [
+  {
+    icon: faEnvelope,
+    url: "mailto: hello@example.com",
+  },
+  {
+    icon: faGithub,
+    url: "https://github.com",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com",
+  },
+  {
+    icon: faMedium,
+    url: "https://medium.com",
+  },
+  {
+    icon: faStackOverflow,
+    url: "https://stackoverflow.com",
+  },
+];
+
+const Header = () => {
+  const handleClick = (anchor) => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
+  return (
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      right={0}
+      translateY={0}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      backgroundColor="#18181b"
+    >
+      <Box color="white" maxWidth="1280px" margin="0 auto">
+        <HStack
+          px={16}
+          py={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <nav>
+            {/* Add social media links based on the `socials` data.map function is used to iterate through each social media entry in the socials array. For each entry, an anchor (<a>) tag is generated with the href attribute pointing to the corresponding social media URL. The target="_blank" and rel="noopener noreferrer" attributes are used to open the link in a new tab securely. */}
+            <HStack spacing={4}>
+              {socials.map((social, index) => (
+                <a
+                  key={index}
+                  href={social.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={social.icon} size="2x" />
+                </a>
+              ))}
+            </HStack>
+          </nav>
+          <nav>
+            <HStack spacing={8}>
+              {/* Add links to Projects and Contact me section. two anchor (<a>) tags are added for the "Projects" and "Contact Me" sections. The href attribute is set to the corresponding section IDs (projects-section and contactme-section). Additionally, the onClick event handler is added to each link, calling the handleClick function with the appropriate anchor. This will ensure smooth scrolling to the respective sections when clicking on the links.*/}
+              <a
+                href="/#projects-section"
+                onClick={handleClick("projects")}
+              >
+                Projects
+              </a>
+              <a
+                href="/#contactme-section"
+                onClick={handleClick("contactme")}
+              >
+                Contact Me
+              </a>
+            </HStack>
+          </nav>
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
